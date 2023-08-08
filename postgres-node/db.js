cda74caabe4056e838cc51d964506083bc40a6f7
marco belahouane <nfk18@hotmail.co.uk>
2023-08-08 20:43:50 +0100

    put a password

--- postgres-node/db.js
+++ postgres-node/db.js
@@ -4,7 +4,8 @@ const pool = new Pool({
   user: process.env.DB_USER || 'marcobelahouane',
   host: process.env.DB_HOST || 'localhost',
   database: process.env.DB_NAME || 'blog',
-  port: process.env.DB_PORT || 5432
+  port: process.env.DB_PORT || 5432,
+  password: process.env.DB_PASSWORD || ''
 })
 
 module.exports = pool
