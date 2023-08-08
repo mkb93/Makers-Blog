ea559a201f794fcdf2e65f4388b7f3ee3d5fb7b9
marco belahouane <nfk18@hotmail.co.uk>
2023-08-08 15:13:07 +0100

    restful api created

--- postgres-node/db.js
+++ postgres-node/db.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  user: 'julianfesting',
+  user: 'marcobelahouane',
   host: 'localhost',
   database: 'blog',
   port: 5432
--- postgres-node/index.js
+++ postgres-node/index.js
@@ -11,13 +11,50 @@ app.use(express.json())
 // create a post
 app.post("/posts", async (req, res) => {
   try {
-    console.log(req.body)
+    const {title} = req.body
+    const newPost = await pool.query("INSERT INTO posts (title) VALUES($1) RETURNING *",[title])
+    res.json(newPost.rows[0]);
   } catch (error) {
     console.error(error.message)
   }
 })
 
-
+app.get("/posts", async (req, res) =>{
+  try {
+    const allPosts = await pool.query("SELECT * FROM posts")
+    res.json(allPosts.rows)
+  } catch (error) {
+    console.error(error.message)
+  }
+})
+app.get("/posts/:id", async (req, res) =>{
+  try {
+    const {id} = req.params
+     const post = await pool.query("SELECT * FROM posts WHERE posts_id = $1",[id])
+    res.json(post.rows[0])
+  } catch (error) {
+    console.error(error.message)
+  }
+})
+app.put("/posts/:id", async (req, res) => {
+  try {
+    const {id} = req.params
+    const {title} = req.body
+    const updatePost = await pool.query("UPDATE posts SET title = $1 WHERE posts_id = $2",[title, id])
+    res.json("Post was updated")
+  } catch (error) {
+    console.error(error.message)
+  }
+})
+app.delete("/posts/:id", async (req, res) => {
+  try {
+    const {id} = req.params;
+    const deletePost =  await pool.query("DELETE FROM posts WHERE posts_id = $1", [id])
+    res.json("Post was deleted")
+  } catch (error) {
+    console.error(error.message)
+  }
+})
 app.listen(4000, ()=>{
   console.log('the server is running on 4000')
 })
